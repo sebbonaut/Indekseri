cc7fe0bf7df7446632bd86368983aa3239b92dee
UFZG <UFZG@DESKTOP-089M5VQ>
2023-11-06 15:56:31 +0100

    Add project files.

---
+++ Indekseri2/Program.cs
@@ -0,0 +1,68 @@
+﻿using Indekseri2;
+
+Student A = new("0123456789");
+Console.WriteLine(A);
+
+byte[,] bod =
+{
+    {2,1},
+    {3,2},
+    {4,3}
+};
+Student B = new("1234353679", bod);
+Console.WriteLine(B[3, 1]);
+A[1, 2] = 5;
+Console.WriteLine(A);
+Console.WriteLine(B[1]);
+
+Kolegij Analiza = new("Analiza", "001");
+Analiza[0] = new[] { A, B };
+Analiza[1] = new[]
+{
+    new Student("1234554321", new byte[,]{{1,2},{3,4},{1,2}}),
+    new Student("1234554322", new byte[,]{{2,2},{5,4},{0,2}}),
+    new Student("1234554323", new byte[,]{{4,2},{3,1},{1,3}})
+};
+
+/* Console.WriteLine("Unesite jmbag studenta:");
+var jmbag = Console.ReadLine();
+if (jmbag != null)
+    Console.WriteLine(Analiza[jmbag]);
+
+try
+{
+    Console.WriteLine(Analiza[1][1][1, 6]);
+}
+catch(IndexOutOfRangeException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+int[] niz = { 9, 1, 6, 5, 3 };
+Index i = ^3;
+Console.WriteLine(niz[i]);
+*/
+
+foreach (Student student in Analiza[^1])
+{
+    Console.WriteLine(student);
+}
+
+//Index a = 1;
+//Analiza[a] = new[] { A, B };
+
+int[] niz = { 1, 2, 3, 4, 5 };
+Range r = ^2..;
+foreach (var br in niz[r])
+{
+    Console.WriteLine(br);
+}
+
+Range raspon = 0..1;
+foreach (Student[] grupa in Analiza[raspon])
+{
+    foreach(Student student in grupa)
+    {
+        Console.WriteLine(student);
+    }   
+}
---
+++ Indekseri2/Student.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indekseri2
+{
+    internal class Student
+    {
+        string jmbag;
+        public string Jmbag => jmbag;
+        byte[,] bodovi;
+        public Student(string jmbag, byte[,] bodovi)
+        {
+            this.jmbag = jmbag;
+            this.bodovi = bodovi;
+        }
+        public Student(string jmbag) : this(jmbag,
+            new byte[3, 2])
+        { }
+        public byte this[int i, int j]
+        {
+            get => bodovi[i - 1, j - 1];
+            set => bodovi[i - 1, j - 1] = value;
+        }
+        public string this[int i]
+        {
+            get
+            {
+                int ukupno = 0;
+                for (int j = 0; j < bodovi.GetLength(1); ++j)
+                    ukupno += bodovi[i - 1, j];
+                return $"{jmbag}, DZ{i}: {ukupno} bodova";
+            }
+        }
+        public override string ToString()
+        {
+            string poruka = $"Podaci za studenta {jmbag}:\n";
+            for (int i = 0; i < bodovi.GetLength(0); ++i)
+            {
+                poruka += $"\tDZ{i + 1}: ";
+                for (int j = 0; j < bodovi.GetLength(1); j++)
+                {
+                    poruka += $"\t{bodovi[i, j]}";
+                }
+                poruka += '\n';
+            }
+            return poruka;
+        }
+    }
+    internal class Kolegij
+    {
+        string naziv, sifra;
+        Student[][] rezultati;
+        public string this[string jmbag]
+        {
+            get
+            {
+                foreach (Student[] grupa in rezultati)
+                {
+                    foreach(Student student in grupa)
+                    {
+                        if(student.Jmbag == jmbag)
+                            return student.ToString();
+                    }
+                }
+                return "Nema podataka o studentu.";
+            }
+        }
+        public Kolegij(string naziv, string sifra)
+        {
+            this.naziv = naziv;
+            this.sifra = sifra;
+            rezultati = new Student[2][];
+        }
+        public Student[] this[int i]
+        {
+            get => rezultati[i];
+            set => rezultati[i] = value;
+        }
+        public Student[] this[Index i]
+            => rezultati[i];
+        public Student[][] this[Range r]
+            => rezultati[r];
+    }
+}
